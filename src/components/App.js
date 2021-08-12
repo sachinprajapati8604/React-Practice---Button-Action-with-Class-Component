d3224da04e180c90407cb56c712cc06e820573e2
Sachin Prajapati <53299703+sachinprajapati8604@users.noreply.github.com>
2021-08-12 16:17:32 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,36 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
+	constructor(props) {
 		super(props);
+
+		// Set initial state
+		this.state = { msg: '' }
+
+		// Binding this keyword
+		this.handleClick = this.handleClick.bind(this)
+	}
+
+	handleClick() {
+
+		// Changing state
+		this.setState({ msg: ' Hello, I\'ve learnt to use the full-stack evaluation tool. This makes me so happy' })
 	};
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+	render() {
+		return (
+			<div id="main">
+				{ /* Do not remove this main div!! */}
+				<p>{this.state.msg}</p>
+
+				{/* Set click handler */}
+				<button onClick={this.handleClick}>
+					Click Me!
+				</button>
+			</div>
+		);
+	}
 }
 
 
